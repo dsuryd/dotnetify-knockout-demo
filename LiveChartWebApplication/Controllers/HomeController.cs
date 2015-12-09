93581ac0e256ad6add7ff71ac1b6fee1e7dc1f4e
Dicky Suryadi <dickysuryadi@hotmail.com>
2015-12-08 19:23:44 -0800

    Initial revision.

---
+++ LiveChartWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace LiveChartWebApplication
+{
+   public class HomeController : Controller
+   {
+      public ActionResult Index(string id)
+      {
+         return File(Server.MapPath("/Views/LiveChart.html"), "text/html");
+      }
+   }
+}
