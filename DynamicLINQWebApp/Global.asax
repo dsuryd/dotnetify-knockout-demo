4cb74755337ac0c3cdadbfc9959e257d372a105c
Dicky Suryadi <dickysuryadi@hotmail.com>
2015-12-22 20:52:15 -0800

    Initial revision.

---
+++ DynamicLINQWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DynamicLINQWebApp.MvcApplication" Language="C#" %>
---
+++ DynamicLINQWebApp/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using DotNetify;
+
+namespace DynamicLINQWebApp
+{
+   public class MvcApplication : System.Web.HttpApplication
+   {
+      protected void Application_Start()
+      {
+         AreaRegistration.RegisterAllAreas();
+         RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+         VMController.RegisterAssembly(typeof(MvcApplication).Assembly);
+      }
+   }
+}
