6439be259c35c7ca5315b2ae83b6ad5c8268c641
Dicky Suryadi <dickysuryadi@hotmail.com>
2015-12-22 20:52:38 -0800

    Initial revision.

---
+++ DynamicLINQWebApp/AFITop100Model.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace DynamicLINQWebApp
+{
+   public class MovieRecord
+   {
+      public int Rank { get; set; }
+      public string Movie { get; set; }
+      public int Year { get; set; }
+      public string Cast { get; set; }
+      public string Director { get; set; }
+
+      public static List<MovieRecord> GetData()
+      {
+         var path = HttpContext.Current.Server.MapPath(@"\AFITop100.json");
+         return JsonConvert.DeserializeObject<List<MovieRecord>>( File.ReadAllText( path ) );
+      }
+   }
+
+   public class AFITop100Model
+   {
+      private static List<MovieRecord> _allRecords = MovieRecord.GetData();
+      public static List<MovieRecord> AllRecords {  get { return _allRecords; } }
+   }
+}
