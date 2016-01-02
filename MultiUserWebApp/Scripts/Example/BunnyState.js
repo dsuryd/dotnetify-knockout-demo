2f218a0a2c0c99f005c6bc781ac0c0f4560f71f6
Dicky Suryadi <dickysuryadi@hotmail.com>
2016-01-01 23:10:27 -0800

    Removed unneeded files.

--- MultiUserWebApp/Scripts/Example/BunnyState.js
+++
@@ -1,8 +0,0 @@
-var Coordinate = (function () {
-    function Coordinate(iX, iY, iAngle) {
-        this.X = iX;
-        this.Y = iY;
-        this.Angle = iAngle;
-    }
-    return Coordinate;
-})();
