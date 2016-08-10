007cdaab6395082587eff9d812f7018cdec76a85
Dicky Suryadi <dickysuryadi@hotmail.com>
2016-08-10 08:41:42 -0700

    Add project files.

---
+++ WebApp/wwwroot/scripts/lib/app.js
@@ -0,0 +1,31 @@
+﻿require.config({
+   baseUrl: '/Scripts',
+   paths: {
+      "jquery": "jquery-1.11.3.min",
+      "jquery-ui": "jquery-ui-widget-1.11.4.min",
+      "knockout": "knockout-3.3.0",
+      "ko-mapping": "knockout.mapping-latest",
+      "dotnetify": "dotnetify",
+      "router": "dotnetify.router",
+      "binder": "dotnetify.binder",
+      "binder-polymer": "dotnetify.binder.polymer",
+      "path": "path.min",
+      "signalr": "jquery.signalR-2.2.0.min",
+      "signalr-hub": "/signalr/hubs?"
+   },
+   shim: {
+      "jquery": { exports: "$" },
+      "knockout": { exports: "ko" },
+      "path": { exports: "Path" },
+      "router": ["path"],
+      "signalr": { deps: ["jquery"], exports: "$.connection" },
+      "signalr-hub": ["signalr"],
+      "binder-polymer": ["binder"]
+   }
+});
+
+require(['jquery', 'knockout', 'dotnetify', 'router', 'binder-polymer'], function ($) {
+   $(function () {
+      dotnetify.debug = true;
+   });
+});
