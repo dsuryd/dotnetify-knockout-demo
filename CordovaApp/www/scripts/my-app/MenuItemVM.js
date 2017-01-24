a6b4f65b8cb6b0e0a1a2045c91076d08cb3b2073
Dicky Suryadi <dsuryd@gmail.com>
2017-01-23 22:39:41 -0800

    Applied webpack to CordovaApp.

--- CordovaApp/www/scripts/my-app/MenuItemVM.js
+++
@@ -1,9 +0,0 @@
-var MenuItemVM = (function () {
-    function MenuItemVM() {
-    }
-    // This function is bound to the back icon HTML click event.
-    MenuItemVM.prototype.back = function () {
-        window.history.back();
-    };
-    return MenuItemVM;
-}());
